aa7730f203d7ebfa10db92dbc886149d22bcc5c3
Marc Miller <marc.miller@email.saintleo.edu>
2020-02-21 10:25:44 -0500

    initial commit of base app w/ cra

---
+++ src/main/java/com/dumbafcbts/dafcbts/DafcbtsApplication.java
@@ -0,0 +1,13 @@
+package com.dumbafcbts.dafcbts;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DafcbtsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DafcbtsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/dumbafcbts/dafcbts/DafcbtsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dumbafcbts.dafcbts;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DafcbtsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
